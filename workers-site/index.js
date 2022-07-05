3857ff293d6b14b0080313aee6874e1ce144e248
user <vrebesbyrd@gmail.com>
2022-07-05 18:50:25 +0300

    handle scheduled event

--- workers-site/index.js
+++ workers-site/index.js
@@ -168,12 +168,48 @@ async function excerpt(file) {
   });
 }
 
+var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+function isEmailValid(email) {
+    if (!email)
+        return false;
+
+    if(email.length>254)
+        return false;
+
+    var valid = emailRegex.test(email);
+    if(!valid)
+        return false;
+
+    // Further checking of some things regex can't handle
+    var parts = email.split("@");
+    if(parts[0].length>64)
+        return false;
+
+    var domainParts = parts[1].split(".");
+    if(domainParts.some(function(part) { return part.length>63; }))
+        return false;
+
+    return true;
+}
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleSubmit(request) {
   const json = await request.json();
+
+  const email = json.email;
+  const hash = json.hash;
+  const value = json.value;
+
+  if (isEmailValid(email)) {
+    await SUBSCRIPTIONS.put(hash + ":" + email, "", {
+      metadata: { created: Date.now(), progress: "0/" + value },
+    });
+    console.log('created subscription', hash + ":" + email, value);
+  }
 
   return new Response(
     JSON.stringify({
@@ -188,13 +224,86 @@ async function handleSubmit(request) {
   );
 }
 
+
+async function processSubscription(item) {
+
+  var meta = item.metadata;
+
+  const progress = meta.progress;
+
+  console.log('processing', item.name, progress);
+
+  var left, right;
+  [left, right] = progress.split("/");
+  [left, right] = [parseInt(left), parseInt(right)];
+
+  if (left < right) {
+    meta.progress = "" + (left + 1) + "/" + right;
+    await SUBSCRIPTIONS.put(item.name, "", {
+      metadata: meta,
+    });
+    console.log('email has been sent', item.name);
+  } else {
+    await SUBSCRIPTIONS.delete(item.name);
+    console.log('stopped sending emails', item.name);
+  }
+
+}
+
+
+//https://medium.com/@_jonas/iterate-over-cloudflare-workers-kv-ab7e42330795
+async function listAll() {
+  // list all keys from a given NAMESPACE,
+  let entries = await SUBSCRIPTIONS.list();
+
+  // get cursor, if returned
+  let cursor = entries.cursor;
+
+  // get entries from first call above
+
+  // делаем "map" массива в промисы
+  let promises = entries.keys.map(processSubscription);
+  // ждем когда всё промисы будут выполнены
+  await Promise.all(promises);
+
+  // as long as `entries.list_complete` is false, loop over the next block
+  while (!entries.list_complete) {
+    let next_value = await SUBSCRIPTIONS.list({cursor: cursor});
+    // update `entries.list_complete` to break the loop
+    // as soon as we've got all keys
+    entries.list_complete = next_value.list_complete;
+    // update cursor for the next call
+    cursor = next_value.cursor
+
+    promises = next_value.keys.map(processSubscription);
+    await Promise.all(promises);
+  }
+}
+
 async function handleScheduled(event) {
   // Write code for updating your API
   switch (event.cron) {
     // You can set up to three schedules maximum.
     case '*/10 * * * *':
+      console.log('case */10 * * * *');
       // Every ten minutes
       // await updateAPI2();
+
+      // 1. maintain collections SUBSCRIPTIONS
+      // where key is hash:email and value is in form 0/45
+      // where 0 is the number of sent emails out of 45 total
+      // {"frf3asda4fdf:michel@gmail.com": "12/30"}
+      // for now do not support the part "each next only after you read the previous"
+      // but it could be done by adding a handler which stores the read event in the metadata for this key
+      // and if there is no metadata for this key, then the next email doesn't have to be sent
+      // 1.1. for mvp it's enough to just send emails on schedule
+      // 2. so after sending an email, the counter is incremented until it hits the <total> value
+      // 3. after that, we remove this key from collection
+
+
+      await listAll();
+
+
       break;
   }
   console.log('cron processed');
